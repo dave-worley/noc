c4c84f0a7ffec2a16d0bd815826ba4087d943065
David Worley <davidworley@gmail.com>
2015-09-24 11:10:59 -0700

    AnimatedEntity definition

--- The Nature of Code/ex01.3/js/main.js
+++ The Nature of Code/ex01.3/js/main.js
@@ -1,5 +1,44 @@
 (function () {
+    /*
+
+      Wraps a Snap shape with an update function, some utilities and useful state.
+
+      entity: a Snap shape, group, text, etc.
+      update: a function called when rendering the shape
+      forces: a list of force vectors such as velocity, friction, wind, gravity, etc.
+
+    */
+
+    function AnimatedEntity (entity, forces) {
+        this.e = entity;
+        this.forces = forces;
+        return this;
+    };
+    AnimatedEntity.prototype.update = function () {
+        var self = this;
+        _.each(forces, function (force) {
+            self.updatePosition();
+        });
+        return this;
+    };
+    AnimatedEntity.prototype.updatePosition = function (nv) { // expects a vector
+        if (!_.isUndefined(self.e.cx)) { // this is a circle
+            self.e.cx += nv.x;
+            self.e.cy += nv.y;
+        } else {
+            self.e.x += nv.x;
+            self.e.y += nv.y;
+        }
+        return this;
+    };
+
     var s = Snap("#snapnature"),
+    entities = [],
+    createAnimatedEntity = function (entityList, shape) {
+        entityList.push(new AnimatedEntity(shape, function () {
+
+        }));
+    },
     center = new Victor(
         Math.floor(s.node.clientWidth/2), 
         Math.floor(s.node.clientHeight/2)
@@ -10,31 +49,13 @@
             + _.random(0, 255)  +  ", " 
             + _.random(0, 255) + ", 1)";
     },
-    mousep = new Victor(0, 0);
-
-    s.mousemove(function (event) {
-        mousep.x = event.x;
-        mousep.y = event.y
-
-        line.attr({
-            x2: event.x,
-            y2: event.y
+    mousep = new Victor(0, 0),
+    render = function () {
+        requestAnimationFrame(render);
+        _.each(entities, function (entity) {
+            entity.update();
         });
-
-    });
-
-
-
-    var updateCircle = function () {
-        s.circle(mousep.x, mousep.y, 10).attr({fill: fillColor});
-        fillColor = newrgb();
-        circle.attr({fill: fillColor});
-        circle.remove();
-        s.add(circle);
     };
-
-    s.mousedown(function (event) {
-        updateCircle();
-    });
+    
 
 })();
