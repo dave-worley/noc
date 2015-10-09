3d7336a8608b780ce4d44c8a78f23fbe3f40d85c
David Worley <davidworley@gmail.com>
2015-10-09 14:54:52 -0700

    offset in stead of clientx/y. lines example.

--- The Nature of Code/ex1.10/js/ex1.10.lines.js
+++ The Nature of Code/ex1.10/js/ex1.10.lines.js
@@ -1,5 +1,5 @@
 (function (util) {
-    var paper = Snap("#ex1-10");
+    var paper = Snap("#ex1-10-lines");
     var mousep = util.vector(0, 0);
     var Mover = function (paper) {
         var psize = {
@@ -9,7 +9,7 @@
         var weightedRGB = function () {
             return "rgba("
                 + _.random(200, 255) + ", "
-                + _.random(100, 200)  +  ", "
+                + _.random(0, 0) + ", "
                 + _.random(0) + ", "
                 + 1 + ")";
         };
@@ -18,48 +18,51 @@
             location: location,
             velocity: util.vector(1, -1),
             maxSpeed: 10,
-            shape: paper.circle(location.x, location.y, _.random(1, 5)).attr({
-                //strokeWidth: 1,
-                //stroke: '#000',
-                fill: weightedRGB(),
-                fillOpacity: _.random(0.1, 0.6)
-            }),
+            line: paper.line(
+                location.x,
+                location.y,
+                mousep.x,
+                mousep.y
+            ).attr({
+                    stroke: weightedRGB()
+                }),
             update: function () {
                 var direction = mousep.clone().subtract(this.location).normalize().multiply(util.vector(0.5, 0.5));
                 this.velocity.add(direction);
                 this.velocity.limit(this.maxSpeed, 0.9);
                 this.location.add(this.velocity);
                 if (_.random(0, 100) < 3) {
-                    this.shape.attr({
-                        fillOpacity: 1
+                    this.line.attr({
+                        strokeOpacity: 1
                     });
                 } else {
-                    this.shape.attr({
-                        fillOpacity: _.random(0.01, 0.6)
+                    this.line.attr({
+                        strokeOpacity: _.random(0.01, 0.6)
                     });
                 }
                 this.checkEdges();
                 this.display();
             },
             checkEdges: function () {
-                var r = parseInt(this.shape.attr('r'));
                 _.each(['x', 'y'], function (axis) {
                     if (this.location[axis] > psize[axis] ||
-                        this.location[axis] < r) {
+                        this.location[axis] < 0) {
                         this.bounce(axis);
                     }
                     if (this.location[axis] > psize[axis]) {
-                        this.location[axis] = psize[axis] - r;
+                        this.location[axis] = psize[axis];
                     }
-                    if (this.location[axis] < r) {
-                        this.location[axis] = r;
+                    if (this.location[axis] < 0) {
+                        this.location[axis] = 0;
                     }
                 }, this);
             },
             display: function () {
-                this.shape.attr({
-                    cx: this.location.x,
-                    cy: this.location.y
+                this.line.attr({
+                    x1: this.location.x,
+                    y1: this.location.y,
+                    x2: mousep.x,
+                    y2: mousep.y
                 });
             },
             bounce: function (axis) {
@@ -77,12 +80,6 @@
                 util.vector(1, 1)
             ];
         CrazyMover.acceleration = util.vector(0.001, 0.001);
-        CrazyMover.shape.remove();
-        //CrazyMover.shape.attr({
-        //    fill: '#f00',
-        //    stroke: '#000',
-        //    r: 10
-        //});
         CrazyMover.maxSpeed = 5;
         CrazyMover.followMe = true;
         CrazyMover.update = function () {
@@ -96,7 +93,7 @@
         return CrazyMover;
     };
 
-    var stage = _.map(_.range(80), function () {
+    var stage = _.map(_.range(40), function () {
         return Mover(paper);
     });
     var crazy = CrazyMover(Mover);
