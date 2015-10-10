60a90dab996cf70da802fea004f0d08ec107180c
David Worley <davidworley@gmail.com>
2015-10-10 11:05:48 -0700

    Prototype in stead of literal. Better performance.

--- The Nature of Code/ex1.10/js/ex1.10.js
+++ The Nature of Code/ex1.10/js/ex1.10.js
@@ -1,105 +1,107 @@
 (function (util) {
     var paper = Snap("#ex1-10");
     var mousep = util.vector(0, 0);
-    var Mover = function (paper) {
-        var psize = {
-            x: paper.node.clientWidth || paper.node.parentNode.clientWidth,
-            y: paper.node.clientHeight || paper.node.parentNode.clientHeight
-        };
-        var weightedRGB = function () {
+    var psize = {
+        x: paper.node.clientWidth || paper.node.parentNode.clientWidth,
+        y: paper.node.clientHeight || paper.node.parentNode.clientHeight
+    };
+
+    function Mover(paper) {
+        this.location = util.vector(_.random(0, psize.x), _.random(0, psize.y));
+        this.velocity = util.vector(1, -1);
+        this.maxSpeed = 10;
+        this.shape = paper.circle(
+            this.location.x,
+            this.location.y,
+            _.random(1, 5)
+        ).attr({
+                fill: this.weightedRGB(),
+                fillOpacity: _.random(0.1, 0.6)
+            });
+        return this;
+    }
+
+    Mover.prototype = {
+        weightedRGB: function () {
             return "rgba("
                 + _.random(200, 255) + ", "
-                + _.random(100, 200)  +  ", "
+                + _.random(100, 200) + ", "
                 + _.random(0) + ", "
                 + 1 + ")";
-        };
-        var location = util.vector(_.random(0, psize.x), _.random(0, psize.y));
-        return {
-            location: location,
-            velocity: util.vector(1, -1),
-            maxSpeed: 10,
-            shape: paper.circle(location.x, location.y, _.random(1, 5)).attr({
-                //strokeWidth: 1,
-                //stroke: '#000',
-                fill: weightedRGB(),
-                fillOpacity: _.random(0.1, 0.6)
-            }),
-            update: function () {
-                var direction = mousep.clone().subtract(this.location).normalize().multiply(util.vector(0.5, 0.5));
-                this.velocity.add(direction);
-                this.velocity.limit(this.maxSpeed, 0.9);
-                this.location.add(this.velocity);
-                if (_.random(0, 100) < 3) {
-                    this.shape.attr({
-                        fillOpacity: 1
-                    });
-                } else {
-                    this.shape.attr({
-                        fillOpacity: _.random(0.01, 0.6)
-                    });
-                }
-                this.checkEdges();
-                this.display();
-            },
-            checkEdges: function () {
-                var r = parseInt(this.shape.attr('r'));
-                _.each(['x', 'y'], function (axis) {
-                    if (this.location[axis] > psize[axis] ||
-                        this.location[axis] < r) {
-                        this.bounce(axis);
-                    }
-                    if (this.location[axis] > psize[axis]) {
-                        this.location[axis] = psize[axis] - r;
-                    }
-                    if (this.location[axis] < r) {
-                        this.location[axis] = r;
-                    }
-                }, this);
-            },
-            display: function () {
-                this.shape.attr({
-                    cx: this.location.x,
-                    cy: this.location.y
-                });
-            },
-            bounce: function (axis) {
-                this.velocity[axis] *= -1;
-                if (!_.isUndefined(this.acceleration)) {
-                    this.acceleration[axis] *= -1;
-                }
-            }
-        };
-    };
-    var CrazyMover = function (Mover) {
-        var CrazyMover = Mover(paper),
-            accelerationBoundary = [
-                util.vector(-1, -1),
-                util.vector(1, 1)
-            ];
-        CrazyMover.acceleration = util.vector(0.001, 0.001);
-        CrazyMover.shape.remove();
-        //CrazyMover.shape.attr({
-        //    fill: '#f00',
-        //    stroke: '#000',
-        //    r: 10
-        //});
-        CrazyMover.maxSpeed = 5;
-        CrazyMover.followMe = true;
-        CrazyMover.update = function () {
-            this.acceleration.randomize(accelerationBoundary[0], accelerationBoundary[1]);
-            this.velocity.add(this.acceleration);
+        },
+        update: function () {
+            var direction = mousep.clone()
+                .subtract(this.location)
+                .normalize()
+                .multiply(util.vector(0.5, 0.5));
+            this.velocity.add(direction);
             this.velocity.limit(this.maxSpeed, 0.9);
             this.location.add(this.velocity);
+            if (_.random(0, 100) < 3) {
+                this.shape.attr({
+                    fillOpacity: 1
+                });
+            } else {
+                this.shape.attr({
+                    fillOpacity: _.random(0.01, 0.6)
+                });
+            }
             this.checkEdges();
             this.display();
-        };
-        return CrazyMover;
+        },
+        checkEdges: function () {
+            var r = parseInt(this.shape.attr('r'));
+            _.each(['x', 'y'], function (axis) {
+                if (this.location[axis] > psize[axis] ||
+                    this.location[axis] < r) {
+                    this.bounce(axis);
+                }
+                if (this.location[axis] > psize[axis]) {
+                    this.location[axis] = psize[axis] - r;
+                }
+                if (this.location[axis] < r) {
+                    this.location[axis] = r;
+                }
+            }, this);
+        },
+        display: function () {
+            this.shape.attr({
+                cx: this.location.x,
+                cy: this.location.y
+            });
+        },
+        bounce: function (axis) {
+            this.velocity[axis] *= -1;
+            if (!_.isUndefined(this.acceleration)) {
+                this.acceleration[axis] *= -1;
+            }
+        }
+    };
+    function CrazyMover() {
+        this.accelerationBoundary = [
+            util.vector(-1, -1),
+            util.vector(1, 1)
+        ];
+        this.acceleration = util.vector(0.001, 0.001);
+        this.maxSpeed = 5;
+        this.followMe = true;
+        return this;
+    };
+    CrazyMover.prototype = new Mover(paper);
+    CrazyMover.prototype.update = function () {
+        this.acceleration.randomize(this.accelerationBoundary[0], this.accelerationBoundary[1]);
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed, 0.9);
+        this.location.add(this.velocity);
+        this.checkEdges();
+        this.display();
     };
 
     var stage = _.map(_.range(80), function () {
-        return Mover(paper);
+        return new Mover(paper);
     });
-    var crazy = CrazyMover(Mover);
+    var crazy = new CrazyMover(Mover);
+    crazy.shape.remove();
     stage.push(crazy);
 
     var render = function () {
--- The Nature of Code/ex1.10/js/ex1.10.lines.js
+++ The Nature of Code/ex1.10/js/ex1.10.lines.js
@@ -1,102 +1,105 @@
 (function (util) {
     var paper = Snap("#ex1-10-lines");
     var mousep = util.vector(0, 0);
-    var Mover = function (paper) {
-        var psize = {
-            x: paper.node.clientWidth || paper.node.parentNode.clientWidth,
-            y: paper.node.clientHeight || paper.node.parentNode.clientHeight
-        };
-        var weightedRGB = function () {
+    var psize = {
+        x: paper.node.clientWidth || paper.node.parentNode.clientWidth,
+        y: paper.node.clientHeight || paper.node.parentNode.clientHeight
+    };
+
+    function Mover(paper) {
+        this.location = util.vector(_.random(0, psize.x), _.random(0, psize.y));
+        this.velocity = util.vector(1, -1);
+        this.maxSpeed = 10;
+        this.line = paper.line(
+            location.x,
+            location.y,
+            mousep.x,
+            mousep.y
+        ).attr({
+                stroke: this.weightedRGB()
+            });
+    }
+    Mover.prototype = {
+        weightedRGB: function () {
             return "rgba("
                 + _.random(200, 255) + ", "
                 + _.random(0, 0) + ", "
                 + _.random(0) + ", "
                 + 1 + ")";
-        };
-        var location = util.vector(_.random(0, psize.x), _.random(0, psize.y));
-        return {
-            location: location,
-            velocity: util.vector(1, -1),
-            maxSpeed: 10,
-            line: paper.line(
-                location.x,
-                location.y,
-                mousep.x,
-                mousep.y
-            ).attr({
-                    stroke: weightedRGB()
-                }),
-            update: function () {
-                var direction = mousep.clone().subtract(this.location).normalize().multiply(util.vector(0.5, 0.5));
-                this.velocity.add(direction);
-                this.velocity.limit(this.maxSpeed, 0.9);
-                this.location.add(this.velocity);
-                if (_.random(0, 100) < 3) {
-                    this.line.attr({
-                        strokeOpacity: 1
-                    });
-                } else {
-                    this.line.attr({
-                        strokeOpacity: _.random(0.01, 0.6)
-                    });
-                }
-                this.checkEdges();
-                this.display();
-            },
-            checkEdges: function () {
-                _.each(['x', 'y'], function (axis) {
-                    if (this.location[axis] > psize[axis] ||
-                        this.location[axis] < 0) {
-                        this.bounce(axis);
-                    }
-                    if (this.location[axis] > psize[axis]) {
-                        this.location[axis] = psize[axis];
-                    }
-                    if (this.location[axis] < 0) {
-                        this.location[axis] = 0;
-                    }
-                }, this);
-            },
-            display: function () {
-                this.line.attr({
-                    x1: this.location.x,
-                    y1: this.location.y,
-                    x2: mousep.x,
-                    y2: mousep.y
-                });
-            },
-            bounce: function (axis) {
-                this.velocity[axis] *= -1;
-                if (!_.isUndefined(this.acceleration)) {
-                    this.acceleration[axis] *= -1;
-                }
-            }
-        };
-    };
-    var CrazyMover = function (Mover) {
-        var CrazyMover = Mover(paper),
-            accelerationBoundary = [
-                util.vector(-1, -1),
-                util.vector(1, 1)
-            ];
-        CrazyMover.acceleration = util.vector(0.001, 0.001);
-        CrazyMover.maxSpeed = 5;
-        CrazyMover.followMe = true;
-        CrazyMover.update = function () {
-            this.acceleration.randomize(accelerationBoundary[0], accelerationBoundary[1]);
-            this.velocity.add(this.acceleration);
+        },
+        update: function () {
+            var direction = mousep.clone().subtract(this.location).normalize().multiply(util.vector(0.5, 0.5));
+            this.velocity.add(direction);
             this.velocity.limit(this.maxSpeed, 0.9);
             this.location.add(this.velocity);
+            if (_.random(0, 100) < 3) {
+                this.line.attr({
+                    strokeOpacity: 1
+                });
+            } else {
+                this.line.attr({
+                    strokeOpacity: _.random(0.01, 0.6)
+                });
+            }
             this.checkEdges();
             this.display();
-        };
-        return CrazyMover;
+        },
+        checkEdges: function () {
+            _.each(['x', 'y'], function (axis) {
+                if (this.location[axis] > psize[axis] ||
+                    this.location[axis] < 0) {
+                    this.bounce(axis);
+                }
+                if (this.location[axis] > psize[axis]) {
+                    this.location[axis] = psize[axis];
+                }
+                if (this.location[axis] < 0) {
+                    this.location[axis] = 0;
+                }
+            }, this);
+        },
+        display: function () {
+            this.line.attr({
+                x1: this.location.x,
+                y1: this.location.y,
+                x2: mousep.x,
+                y2: mousep.y
+            });
+        },
+        bounce: function (axis) {
+            this.velocity[axis] *= -1;
+            if (!_.isUndefined(this.acceleration)) {
+                this.acceleration[axis] *= -1;
+            }
+        }
     };
 
+    function CrazyMover() {
+        this.accelerationBoundary = [
+            util.vector(-1, -1),
+            util.vector(1, 1)
+        ];
+        this.acceleration = util.vector(0.001, 0.001);
+        this.maxSpeed = 5;
+        this.followMe = true;
+    }
+    CrazyMover.prototype = new Mover(paper);
+    CrazyMover.prototype.update = function () {
+        this.acceleration.randomize(this.accelerationBoundary[0], this.accelerationBoundary[1]);
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed, 0.9);
+        this.location.add(this.velocity);
+        this.checkEdges();
+        this.display();
+    };
+
+
+
+
     var stage = _.map(_.range(40), function () {
-        return Mover(paper);
+        return new Mover(paper);
     });
-    var crazy = CrazyMover(Mover);
+    var crazy = new CrazyMover(Mover);
     stage.push(crazy);
 
     var render = function () {
@@ -109,8 +112,8 @@
 
     paper.mousemove(function (evt) {
         if (!crazy.followMe) {
-            mousep.x = _.isUndefined(evt.offsetX) ? evt.clientX : evt.offsetX;
-            mousep.y = _.isUndefined(evt.offsetY) ? evt.clientY : evt.offsetY;
+            mousep.x = _.isUndefined(evt.offsetX) ? evt.layerX : evt.offsetX;
+            mousep.y = _.isUndefined(evt.offsetY) ? evt.layerY : evt.offsetY;
         }
     });
     paper.mouseover(function (evt) {
